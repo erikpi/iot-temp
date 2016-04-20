a3d4fce1aa1e205d525619a3ab29642fd9f31314
jaomi <jaomi@haxx.info>
2016-04-21 01:12:01 +0200

    better date stamps and log formating

--- temptest.sh
+++ temptest.sh
@@ -8,7 +8,7 @@ do
 
 	TEMP=$(sudo ./Adafruit_DHT 11 4 | grep Temp)
 	if [ ! -z "$TEMP" ]; then 
-		(echo -n "$(date '+%D %R') "; echo $TEMP) | awk '{ print $1, $2, $4, $6, $8  }' >> $LOGFILE
+		(echo -n "$(date '+%D %R') "; echo $TEMP) | awk '{ print $1, $2, $3, $5, $7, $9 }' >> $LOGFILE
 
 		 cp -f $LOGFILE /var/www/temperatur.log
 	
