740ba3c9ecc21f2954ea8ea4a44a9c187b0203ec
jaomi <jaomi@haxx.info>
2016-04-20 23:24:55 +0100

    Smarter date format for graph plotting

--- temptest.sh
+++ temptest.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
 LOGFILE=temperatur.log
-rm $LOGFILE
+
 
 while true
 do
+
 	TEMP=$(sudo ./Adafruit_DHT 11 4 | grep Temp)
 	if [ ! -z "$TEMP" ]; then 
-		echo -n "$(date) "  >> $LOGFILE
-		echo $TEMP >> $LOGFILE
-		(echo -n "$(date) "; echo $TEMP) > /var/www/temperatur.log
+		(echo -n "$(date +%D:%H) "; echo $TEMP)
+		awk '{ print $1, $2:$4, $6:$8  }' >> $LOGFILE
+
+		 cp -f $LOGFILE /var/www/temperatur.log
 	
 	sleep 2
 	fi
